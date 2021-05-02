e912f1568621d81a428d00198d01c6b23ba73119
juliettaanastasia <74484044+juliettaanastasia@users.noreply.github.com>
2021-05-02 11:59:16 +0700

    Add files via upload

---
+++ bahanbakus.php
@@ -0,0 +1,229 @@
+class BahanbakuSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('bahanbakus')->insert
+        ([
+            'name' => "Bawang Merah",
+            'description' => "Bawang merah sering digunakan sebagai bumbu bahan masakan untuk hampir setiap jenis makanan di Indonesia",
+            'pricetag' => "Rp. 1.600 / Ons",
+            'price' => 1600,
+            'jenis' => "Rempah",
+            'gambar' => "",
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+        ]);
+    }
+}
+
+class BahanbakuSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('bahanbakus')->insert
+        ([
+            'name' => "Bawang Putih",
+            'description' => "Bawang putih sering digunakan sebagai bumbu bahan masakan untuk hampir setiap jenis makanan di Indonesia",
+            'pricetag' => "Rp. 3.200 / Ons",
+            'price' => 3200,
+            'jenis' => "Rempah",
+            'gambar' => "",
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+        ]);
+    }
+}
+
+class BahanbakuSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('bahanbakus')->insert
+        ([
+            'name' => "Bawang Bombay",
+            'description' => "Bawang bombay sering digunakan sebagai bumbu bahan masakan untuk hampir setiap jenis makanan di Indonesia, dan bisa dijadikan kudapan seperti Onion Ring",
+            'pricetag' => "Rp. 3.800 / Ons",
+            'price' => 3800,
+            'jenis' => "Rempah",
+            'gambar' => "",
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+        ]);
+    }
+}
+
+class BahanbakuSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('bahanbakus')->insert
+        ([
+            'name' => "Jahe",
+            'description' => "Jahe tak hanya dikenal sebagai salah satu obat herbal dengan sejuta manfaat, tetapi juga dikenal sebagai penyedap rasa yang dibutuhkan untuk berbagai macam masakan",
+            'pricetag' => "Rp. 9.800 / Ons",
+            'price' => 9800,
+            'jenis' => "Rempah",
+            'gambar' => "",
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+        ]);
+    }
+}
+
+class BahanbakuSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('bahanbakus')->insert
+        ([
+            'name' => "Cengkeh",
+            'description' => "Cengkeh sangat populer di kalangan bumbu dapur karena khasiatnya dalam memberikan aroma yang sedap pada masakan",
+            'pricetag' => "Rp. 15.000 / Ons",
+            'price' => 15000,
+            'jenis' => "Rempah",
+            'gambar' => "",
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+        ]);
+    }
+}
+
+class BahanbakuSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('bahanbakus')->insert
+        ([
+            'name' => "Cabai Merah",
+            'description' => "Cabai merah sering digunakan sebagai bumbu maupun sambal pada masakan untuk hampir setiap jenis makanan di Indonesia",
+            'pricetag' => "Rp. 5.000 / Ons",
+            'price' => 5000,
+            'jenis' => "Rempah",
+            'gambar' => "",
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+        ]);
+    }
+}
+
+class BahanbakuSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('bahanbakus')->insert
+        ([
+            'name' => "Cabai Hijau",
+            'description' => "Cabai hijau sering digunakan sebagai bumbu maupun sambal pada masakan untuk hampir setiap jenis makanan di Indonesia",
+            'pricetag' => "Rp. 5.500 / Ons",
+            'price' => 5500,
+            'jenis' => "Rempah",
+            'gambar' => "",
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+        ]);
+    }
+}
+
+class BahanbakuSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('bahanbakus')->insert
+        ([
+            'name' => "Tomat",
+            'description' => "Tomat sangat cocok dijadikan sebagai pendamping menu utama serta sebagai minuman penyegar tubuh, dan juga bisa dimakan langsung",
+            'pricetag' => "Rp. 2.500 / Ons",
+            'price' => 2500,
+            'jenis' => "Rempah",
+            'gambar' => "",
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+        ]);
+    }
+}
+
+class BahanbakuSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('bahanbakus')->insert
+        ([
+            'name' => "Tomat Hijau",
+            'description' => "Tomat hijau selain menjadi bahan masakan, juga memiliki banyak manfaat bagi kesehatan tubuh dan kulit",
+            'pricetag' => "Rp. 1.500 / Ons",
+            'price' => 1500,
+            'jenis' => "Rempah",
+            'gambar' => "",
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+        ]);
+    }
+}
+
+class BahanbakuSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('bahanbakus')->insert
+        ([
+            'name' => "Kentang",
+            'description' => "Kentang sering digunakan sebagai hidangan baik menjadi menu utama yang bisa menggantikan nasi, karena juga mengandung karbohidrat, maupun untuk cemilan saja",
+            'pricetag' => "Rp. 2.000 / Ons",
+            'price' => 2000,
+            'jenis' => "Bahan Masakan",
+            'gambar' => "",
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+        ]);
+    }
+}
