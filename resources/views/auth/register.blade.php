3c0c4f8a199d5879d0c336e9d0108748001a38f9
Haki <57068224+GhiffariHaki@users.noreply.github.com>
2021-05-03 11:51:08 +0700

    Adding Class Role

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -40,6 +40,17 @@
                         </div>
 
                         <div class="form-group row">
+                            <label for="role" class="col-md-4 col-form-label text-md-right">{{ __('Role') }}</label>
+
+                            <div class="col-md-6">
+                                <br><br><select>
+                                    <option value= Seller> Buyer </option>
+                                    <option value= Buyer> Seller </option>
+                                </select><br><br>
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
                             <div class="col-md-6">
