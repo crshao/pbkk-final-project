810148dbe0b4be569993f888843e6e57c784e274
Haki <57068224+GhiffariHaki@users.noreply.github.com>
2021-06-21 04:48:38 +0700

    adding navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,16 +15,21 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Bungee" rel="stylesheet">
 
     <!-- Styles -->
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.0/css/all.min.css'>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <nav class="navbar navbar-expand-md navbar-dark bg-success shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                <a class="navbar-brand" href="{{ url('/home') }}">
+                    <img src="{{ asset('img/Sayurku.png')}}" width="40" height="40" class="d-inline-block align-bottom" alt="">
+                    <span style="font-family: 'Bungee; font-size: 20px">Sayurku</span>
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -39,6 +44,15 @@
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ url('/home') }}"><span class="fas fa-home"></span>Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ url('/bahanbaku') }}"><span class="fas fa-leaf"></span>Bahan Baku</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ url('/resep') }}"><span class="fas fa-receipt"></span>Resep</a>
+                        </li>
                         @guest
                             @if (Route::has('login'))
                                 <li class="nav-item">
