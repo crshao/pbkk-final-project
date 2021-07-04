c1cf9d407bd46cad87078ae8232c5e2b53c76492
Nazhwa Ameera <68385532+nazhwaameera@users.noreply.github.com>
2021-07-04 10:46:34 +0700

    menambahkan about us

---
+++ resources/views/aboutus/about.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.template')
+@section('content')
+<div class="container">
+  <div class="row">
+    <div class="col">
+      <h1>About Us</h1>
+        <img src="{{ ('img/market.png ') }}">
+        <hr>
+        <p>Dalam masa pandemi seperti saat ini, kita diharuskan untuk mengisolasi diri. Hal tersebut tentunya menyulitkan kita, khususnya bagi ibu rumah tangga untuk berbelanja keperluan dapur. Maka dari itu, aplikasi ini dibuat dengan harapan untuk bisa mengatasi masalah tersebut, dan para konsumen pun bisa berbelanja dengan aman tanpa perlu memikirkan akan terjangkit wabah. Disamping itu, tujuan kami mendirikan aplikasi ini adalah untuk mengurangi food waste dan meminimalisir adanya malnutrisi. Kami berharap bahwa aplikasi ini dapat memudahkan para konsumen untuk berbelanja sayur dengan cara yang lebih instan, mudah dan cepat, serta berdampak positif terhadap hubungan antara penjual sayur dan konsumen, terutama ibu rumah tangga. Mereka bisa menjadi mitra yang baik dan saling terhubung dalam kegiatan jual beli secara praktis yang sangat dibutuhkan di era digital seperti ini.</p>
+        <hr>
+    </div>
+  </div>
+</div>
+@endsection
